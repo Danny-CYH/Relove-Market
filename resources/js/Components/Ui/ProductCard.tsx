717f940fa78fecf8414efe3d673972f5ee5b3623
Danny CYH <chengyangho14@gmail.com>
2026-06-30 19:43:00 +0800

    refactor: remove tooltip for color display in ProductCard component

--- resources/js/Components/Ui/ProductCard.tsx
+++ resources/js/Components/Ui/ProductCard.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { Link } from "@inertiajs/react";
 import { usePage } from "@inertiajs/react";
 
+// Helper Functions
 import { GetColorValue } from "../HelperFunction/GetColorValue";
 import { getVariantDetails } from "../HelperFunction/GetVariantDetails";
 import { getDisplayPrice } from "../HelperFunction/GetDisplayPrice";
@@ -157,11 +158,6 @@ export function ProductCard({ product, selected_variant }) {
                                         <span className="text-xs text-gray-700 font-medium capitalize">
                                             {variantDetails.color}
                                         </span>
-                                    </div>
-                                    {/* Tooltip on hover for accessibility */}
-                                    <div className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 px-2 py-1 bg-gray-800 text-white text-xs rounded opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none whitespace-nowrap z-20">
-                                        Color: {variantDetails.color}
-                                        <div className="absolute top-full left-1/2 transform -translate-x-1/2 border-4 border-transparent border-t-gray-800"></div>
                                     </div>
                                 </div>
                             )}
