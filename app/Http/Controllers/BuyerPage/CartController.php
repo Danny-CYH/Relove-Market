34f75d0c86eac6b72f710c49d1ecdf4356acf036
Danny CYH <chengyangho14@gmail.com>
2026-07-05 13:09:29 +0800

    refactor: use UserTrait to call the user id

--- app/Http/Controllers/BuyerPage/CartController.php
+++ app/Http/Controllers/BuyerPage/CartController.php
@@ -5,19 +5,14 @@ namespace App\Http\Controllers\BuyerPage;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CartResource;
 use App\Models\Cart;
+use App\Traits\UserTrait;
 use Illuminate\Http\Request;
 use Exception;
 
 class CartController extends Controller
 {
-    protected $user_id;
+    use UserTrait;
 
-    public function __construct()
-    {
-        $this->user_id = session('user_id');
-    }
-
-    // Code for getting all the cart item for specific user
     public function getAllCart()
     {
         $all_cart = Cart::with([
@@ -26,31 +21,21 @@ class CartController extends Controller
             "productImage",
             "productVariant",
         ])
-            ->where("user_id", $this->user_id)
+            ->where("user_id", $this->userId())  // ✅ 改成 $this->userId()
             ->get();
 
         return CartResource::collection($all_cart);
     }
 
-    // Code for validate that did the user have ever like the product before
     public function getCart($product_id)
     {
         try {
-            $is_cart = Cart::where(
-                "product_id",
-                $product_id
-            )
-                ->where("user_id", $this->user_id)
+            $is_cart = Cart::where("product_id", $product_id)
+                ->where("user_id", $this->userId())
                 ->first();
 
-            if ($is_cart != null) {
-                return response()->json([
-                    "is_cart" => true
-                ]);
-            }
-
             return response()->json([
-                "is_cart" => false
+                "is_cart" => $is_cart ? true : false
             ]);
         } catch (Exception $e) {
             return response()->json([
@@ -59,38 +44,33 @@ class CartController extends Controller
         }
     }
 
-    // Code for storing the product as a wishlist
     public function storeWishlist(Request $request)
     {
         $product_id = $request->input("product_id");
 
         try {
-            $wishlistData = [
-                'user_id' => $this->user_id,
+            $cartData = [
+                'user_id' => $this->userId(),  // ✅ 改成 $this->userId()
                 'product_id' => $product_id,
             ];
 
-            // Add variant data if provided
             if ($request->has('selected_variant')) {
-                $wishlistData['selected_variant'] = json_encode($request->selected_variant);
+                $cartData['selected_variant'] = json_encode($request->selected_variant);
             }
 
-            // Check if already in cart
-            $existingCart = Cart::where('user_id', $this->user_id)
+            $existingCart = Cart::where('user_id', $this->userId())
                 ->where('product_id', $product_id)
                 ->first();
 
             if ($existingCart) {
-                // Update existing cart item with new variant/options
-                $existingCart->update($wishlistData);
+                $existingCart->update($cartData);
             } else {
-                // Create new cart item
-                Cart::create($wishlistData);
+                Cart::create($cartData);
             }
 
             return response()->json([
                 'successMessage' => 'Product added to cart successfully',
-                "data" => $wishlistData
+                "data" => $cartData
             ]);
         } catch (Exception $e) {
             return response()->json(["errorMessage" => $e->getMessage()]);
@@ -104,7 +84,7 @@ class CartController extends Controller
             'variant_data' => 'required|array'
         ]);
 
-        $cartItem = Cart::where('user_id', $this->user_id)
+        $cartItem = Cart::where('user_id', $this->userId())
             ->where('product_id', $request->product_id)
             ->first();
 
@@ -119,17 +99,16 @@ class CartController extends Controller
         return response()->json(['success' => false], 404);
     }
 
-    // Code for removing the product from cart (Can removed more than 1 at one time)
     public function removeCart(Request $request)
     {
         try {
-            $productIds = $request->input('product_id'); // array from frontend
+            $productIds = $request->input('product_id');
 
             if (!is_array($productIds)) {
-                $productIds = [$productIds]; // make sure it's an array
+                $productIds = [$productIds];
             }
 
-            $deletedCount = Cart::where('user_id', $this->user_id)
+            $deletedCount = Cart::where('user_id', $this->userId())
                 ->whereIn('product_id', $productIds)
                 ->delete();
 
